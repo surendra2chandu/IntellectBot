cd33c022b8570539f87b2e650b3dc1faa9bf31f4
surendra2chandu <surendra2chandu@gmail.com>
2025-06-10 07:01:25 -0700

    Update Prompts.py

--- src/conf/Prompts.py
+++ src/conf/Prompts.py
@@ -28,6 +28,24 @@ Intent:"""
 
 
 RAG_PROMPT1 = """
+Label above rephrased question from a conversation with an intent. Reply ONLY with the name of the intent.
+
+The intent should be one of the following:
+- greet
+- status
+- unknown
+
+Message: {query1}    # Got from vector database
+Intent:  {intent1}   # Got from vector database
+Message: {query2}    # Got from vector database
+Intent:  {intent2}   # Got from vector database
+
+query: {query}    # Got from user input
+Intent:
+
+"""
+
+RAG_PROMPT2 = """
 step1:
 Rephrase the question: include only the core ask and output the ask in a clear and appropriate 'how to' or 'what is' or 'why is' question. ONLY output the rephrased question.
 Question: {query}
@@ -45,6 +63,7 @@ Intent:  {intent1}   # Got from vector database
 Message: {query2}    # Got from vector database
 Intent:  {intent2}   # Got from vector database
 
+query: {query}    # Got from user input
 Intent:
 
 """
